4dc6afe65dc4f1b76d2355657a1163d5b6d7ffc9
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-03-25 11:34:49 -0300

    Atualizando o conceito de estado com o useState

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,14 @@
+import { Button } from './components/Button'
+
 function App() {
   return (
-   <h1>Hello World!</h1>
+    <>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </>
   );
 }
 
---
+++ front/src/components/Button.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export function Button() {
+    const [counter, setCounter] = useState(0);
+
+    function increment() {
+        setCounter(counter + 1);
+        console.log(setCounter);
+    }
+
+    return (
+        <button onClick={increment}>{counter}</button>
+    )
+}
