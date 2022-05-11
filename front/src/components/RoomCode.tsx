a24772dc582b533584bf4788873f6d5c9ea9878b
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-05-10 23:03:37 -0300

    adding new features

--- front/src/components/RoomCode.tsx
+++ front/src/components/RoomCode.tsx
@@ -2,13 +2,21 @@ import copyImg from '../assets/images/copy.svg'
 
 import '../styles/roomCode.scss'
 
-export function RoomCode() {
+type RoomCodeProps = {
+  code: string;
+}
+
+export function RoomCode(props: RoomCodeProps) {
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code);
+  }
+
   return (
-    <button className="room-code">
+    <button className="room-code" onClick={copyRoomCodeToClipboard}>
       <div>
         <img src={copyImg} alt="Copy room code" />
       </div>
-      <span>Room #-N1ZYw-kDUVkb7h1zoZF</span>
+      <span>Room #c0d3</span>
     </button>
   )
 }
