70cbb8833e652af5a12152918ac3ce5ca1c58210
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-05-12 23:36:54 -0300

    adding new page at the context

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,6 +4,7 @@ import { AuthContextProvider } from './contexts/AuthContext'
 import { Home } from './pages/Home'
 import { Room } from './pages/Room'
 import { NewRoom } from './pages/NewRoom'
+import { AdminRoom } from './pages/AdminRoom'
 
 function App() {
 
@@ -14,6 +15,8 @@ function App() {
           <Route exact path='/' component={Home}/>
           <Route path='/rooms/new' component={NewRoom}/>
           <Route path='/rooms/:id' component={Room}/>
+
+          <Route path='/admin/rooms/:id' component={AdminRoom}/>
         </Switch>
       </Router>
     </AuthContextProvider> 
---
+++ front/src/pages/AdminRoom.tsx
@@ -0,0 +1,79 @@
+import { FormEvent, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { database } from '../services/firebase'
+import { useAuth } from '../hooks/useAuth'
+import { useRoom } from '../hooks/useRoom'
+
+import { RoomCode } from '../components/RoomCode'
+import { Question } from '../components/Question'
+import { Button } from '../components/Button'
+
+import logoImg from '../assets/images/logo.svg'
+
+import '../styles/room.scss'
+
+type RoomParams = {
+  id: string
+}
+
+export function AdminRoom() {
+  const { user } = useAuth()
+  const params = useParams<RoomParams>()
+  const [newQuestion, setNewQuestion] = useState('')
+  
+  const roomCode = params.id
+
+  const { title, questions } = useRoom(roomCode)
+
+  async function handleSendQuestion(event: FormEvent) {
+    event.preventDefault()
+
+    if (newQuestion.trim() === '') return
+
+    if (!user) throw new Error('You must be logged in')
+    
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.id,
+        avatar: user.avatar
+      },
+      isHighlighted: false,
+      isAnswered: false
+    }
+
+    await database.ref(`rooms/${roomCode}/questions`).push(question)
+
+    setNewQuestion('')
+  }
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="letmeask" />
+          <RoomCode code={roomCode}/>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>{title} room's</h1>
+          { questions.length > 0 && <span>{questions.length} question(s)</span> }
+        </div>
+ 
+        <div className="question-list">
+          { questions.map( question => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              />
+            );
+          })}
+        </div>
+      </main>
+    </div>
+  );
+}
